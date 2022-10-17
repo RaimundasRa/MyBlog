546bc85a7ea85342039535dfe299f5de5260473e
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2022-10-17 18:05:45 +0100

    added ost views to database, minor styling changes throught

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_120746) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_164758) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
