d8d9acdb9e851f5c7f539a7f8061d24502331850
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2022-11-01 15:42:00 +0000

    seed file and index styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,22 +16,34 @@ User.create(email: "jon@gmail.com", password: "123456", password_confirmation: "
 User.create(email: "gale@gmail.com", password: "123456", password_confirmation: "123456")
 User.create(email: "susana@gmail.com", password: "123456", password_confirmation: "123456")
 
-3.times do
-  Post.create(title: Faker::Movies::HitchhikersGuideToTheGalaxy.character, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 1)
-end
-
-4.times do
-  Post.create(title: Faker::Movies::HitchhikersGuideToTheGalaxy.character, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 2)
+2.times do
+  Post.create(title: Faker::Fantasy::Tolkien.location, description: Faker::Fantasy::Tolkien.poem, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 1)
 end
 
 2.times do
+  Post.create(title: Faker::Movies::HarryPotter.character, description: Faker::Movies::HarryPotter.quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 1)
+end
+
+4.times do
+  Post.create(title: Faker::TvShows::Seinfeld.business, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 2)
+end
+
+2.times do
+  Post.create(title: Faker::TvShows::GameOfThrones.house, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 3)
+end
+
+3.times do
   Post.create(title: Faker::Movies::HitchhikersGuideToTheGalaxy.character, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 3)
 end
 
 6.times do
-  Post.create(title: Faker::Movies::HitchhikersGuideToTheGalaxy.character, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 4)
+  Post.create(title: Faker::TvShows::GameOfThrones.house, description: Faker::TvShows::BigBangTheory.quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 4)
 end
 
 4.times do
   Post.create(title: Faker::Movies::HitchhikersGuideToTheGalaxy.character, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, user_id: 5)
+end
+
+4.times do
+  Post.create(title: Faker::TvShows::Seinfeld.business, description: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote, body: Faker::TvShows::GameOfThrones.quote, user_id: 5)
 end
